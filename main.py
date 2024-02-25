df57f3d06a18b188d0b324f52d311f5699a4782e
PRAPTI220098 <140289767+PRAPTI220098@users.noreply.github.com>
2024-02-25 12:05:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,27 @@ import pywebio.output as output
 import pywebio.input as input
 import pywebio
 import requests,time
-c = 0
-f = 0
+
+count = {}
+
 def MyApp():
-    global f, c
-    skc = input.input_group('• Call Bomber By: SANCHIT 💸', [input.input(" • Enter Your Number (without +91) ", name="SK")]);SK = str(skc["SK"])
+    skc = input.input_group('• Call Bomber By: SANCHIT 💸', [input.input(" • Enter Your Number (without +91) ", name="SK")])
+    SK = str(skc["SK"])
+
+    if SK not in count:
+        count[SK] = {"successful": 0, "failed": 0}
+
     while True:
         req = requests.get(f"https://bomber-tools.xyz/?mobile={SK}&accesskey=BomberSmm&submit=Submit").text
+
         if 'started' in req:
-            c += 1
-            output.put_html(f'<h3>✅ Bombing On - Count: {c}</h3><i>    BY: SANCHIT</i>')
-            time.sleep(.1)
+            count[SK]["successful"] += 1
+            output.put_html(f'<h3>✅ Bombing On - Count: {count[SK]["successful"]}</h3><i>    BY: SANCHIT</i>')
+            time.sleep(0.1)
         else:
-            f += 1
-            output.put_html(f'<h3>❌ Bombing Off - Count: {f}</h3><i>    BY: SANCHIT</i>')
-            time.sleep(.1)
+            count[SK]["failed"] += 1
+            output.put_html(f'<h3>❌ Bombing Off - Count: {count[SK]["failed"]}</h3><i>    BY: SANCHIT</i>')
+            time.sleep(0.1)
 
 if __name__ == '__main__':
     pywebio.start_server(MyApp, port=8086, debug=True)
